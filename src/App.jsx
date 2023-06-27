dd60edcb1b0ea61b6096111e820bc5206a84330e
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-27 04:23:30 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const url = "https://api.github.com/users";
+  const [data, setData] = useState(null);
+  useEffect(() =>{
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  }, [])
+if(!data){
+  return <h1>Loading....</h1>
+}
   return (
     <>
-      <h1>hello</h1>
+      <ul className="sect">
+        {
+          data.map(user =>{
+              const {id, login, avatar_url, html_url} = user
+            return(
+              <li key={id}>
+                  <img src={avatar_url} alt="" />
+                  <h1>{login}</h1>
+                  <a href={html_url}>profile</a>
+              </li>
+            )
+          })
+        }
+      </ul>
     </>
   )
 }
