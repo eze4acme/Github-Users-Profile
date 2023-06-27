b508c06d7cadbc23af97e0712be70d30b20e0e80
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-27 23:55:03 +0100

    update complete
    update complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const url = "https://api.github.com/users";
   const [data, setData] = useState(null);
-  useEffect(() =>{
+  useEffect(() => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((error) => console.log(error));
-  }, [])
-if(!data){
-  return <h1>Loading....</h1>
-}
+  }, []);
+  if (!data) {
+    return <h1>Loading....</h1>;
+  }
   return (
-    <>
-        <h1>Github Users profile</h1>
+    <section className="section">
+      <h1 className="title">Github Users profile</h1>
       <ul className="sect">
-        {
-          data.map(user =>{
-              const {id, login, avatar_url, html_url} = user
-            return(
-              <li key={id}>
-                  <img src={avatar_url} alt="" />
-                  <h1>{login}</h1>
-                  <a href={html_url}>profile</a>
-              </li>
-            )
-          })
-        }
+        {data.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id} className="setup">
+              <img src={avatar_url} alt="" />
+              <div className="text-section">
+                <h1>{login}</h1>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
       </ul>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
